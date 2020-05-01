c2e7ad4c7d884afe7c904116c8d4dbc10b868004
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-05-01 02:01:15 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,21 +1,32 @@
 require 'pry'
 
+
 class String
 
   def sentence?
-   if "HI!".end_with?("!")
-   
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
-
+  
   def exclamation?
-
+    self.end_with?("!")
+  end
+  
+  def count_sentences
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
+  end
+  
+  def count_sentences_words
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+   
   end
 
-  def count_sentences
+end
+
+"Well, I don't know, is this a sentence?? ??? What about this?".count_sentences_words
 
   end
 end
